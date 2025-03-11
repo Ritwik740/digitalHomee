31ff8b360f53c6c473eb23a40c55ab0a95b652a3
RITWIK MISHRA <ritwikmishrastudent@gmail.com>
2025-03-11 10:29:05 +0530

    debugged

--- digitalHomeez/src/components/Blogs.jsx
+++ digitalHomeez/src/components/Blogs.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./blogs.css";
+import "./Blogs.css";
 
 const blogPosts = [
   {
--- digitalHomeez/src/components/Navbar.jsx
+++ digitalHomeez/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./navbar.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
