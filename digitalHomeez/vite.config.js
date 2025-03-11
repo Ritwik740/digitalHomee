e64c0bdc3cb1fa8dc14bfb06aeef93e5076dea32
RITWIK MISHRA <ritwikmishrastudent@gmail.com>
2025-03-11 15:13:48 +0530

    modified vite config

--- digitalHomeez/vite.config.js
+++ digitalHomeez/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure correct base path
   server: {
-    historyApiFallback: true, // Redirects all routes to index.html
+    historyApiFallback: true, // Redirect all routes to index.html
+  },
+  build: {
+    outDir: 'build', // Ensure correct output directory
   },
 });
